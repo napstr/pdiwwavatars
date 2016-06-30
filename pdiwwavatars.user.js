da05e92109306305cbe3e50d89e00177af4f2268
npstr <napstr@users.noreply.github.com>
2016-06-30 22:18:02 +0200

    v

--- pdiwwavatars.user.js
+++ pdiwwavatars.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pdiwwavatars
 // @namespace    http://tampermonkey.net/
-// @version      0.5.4
+// @version      0.6.5
 // @description  Changes avatars for users in Paradox Interactive forums
 // @author       https://github.com/napstr
 // @grant        none
