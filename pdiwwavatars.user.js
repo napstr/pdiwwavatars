354a5c145e4204ba3c7e14ad8bbfa8a2f1d37191
npstr <napstr@users.noreply.github.com>
2016-06-30 17:22:20 +0200

    moar avatars

--- pdiwwavatars.user.js
+++ pdiwwavatars.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pdiwwavatars
 // @namespace    http://tampermonkey.net/
-// @version      0.2.2
+// @version      0.2.3
 // @description  Changes avatars for users in Paradox Interactive forums
 // @author       https://github.com/napstr
 // @grant        none
@@ -18,6 +18,12 @@ var numbers = ["1072860",//npstr
                "1022496", //jackson
                "448345",//alxeu
                "1056425", //duke dan
+               "91719", //euroo7
+               "889046", //madchemist
+               "928304", //2knikk
+               "1024075", //wombat
+               "57094", //k-59
+               "849376", //rovsea
               ];
 var pics = ["https://i.imgur.com/vQNKh5v.jpg",
             "https://i.imgur.com/4hgEPHc.png",
@@ -26,6 +32,12 @@ var pics = ["https://i.imgur.com/vQNKh5v.jpg",
             "https://i.imgur.com/1q1tu85.gif",
             "https://i.imgur.com/Di8N4nL.png",
             "https://i.imgur.com/yGH71zm.png",
+            "https://i.imgur.com/2uTAHCc.png",
+            "https://i.imgur.com/pmnAV5B.png",
+            "https://i.imgur.com/TEKgwoR.jpg",
+            "https://i.imgur.com/ryb0Cm8.jpg",
+            "https://i.imgur.com/2JP6QML.png",
+            "https://i.imgur.com/WS6PgII.png",
            ];
 
 (function() {
@@ -45,7 +57,7 @@ var pics = ["https://i.imgur.com/vQNKh5v.jpg",
         asd.style.backgroundImage = "url('" + pics[j] + "')";
         asd.style.backgroundSize = "100%";
       }
-      $('img.miniMe')[0].src = pics[j];
+      //$('img.miniMe')[0].src = pics[j]; this needs more work
       
     }
 
@@ -53,6 +65,12 @@ var pics = ["https://i.imgur.com/vQNKh5v.jpg",
     for (var i = 0; i < x.length; i++) {
       var elem = x[i];
       elem.getElementsByTagName("*")[0].src = pics[j];
+    //who replied on own site
+      var asd = elem.getElementsByClassName("img s")[0];
+      if (typeof asd != 'undefined') {
+        asd.style.backgroundImage = "url('" + pics[j] + "')";
+        asd.style.backgroundSize = "100%";
+      }
     }
 
     var x = document.getElementsByClassName("Av" + numbers[j] + "l");
@@ -88,6 +106,14 @@ function updateInOverlays () {
       img.src = pics[j];
       img.height = "192";
       img.width = "192";
+    }
+
+
+    //new post posted
+    var x = document.getElementsByClassName("Av" + numbers[j] + "m");
+    for (var i = 0; i < x.length; i++) {
+      var elem = x[i];
+      elem.getElementsByTagName("*")[0].src = pics[j];
     }
   }
 }
